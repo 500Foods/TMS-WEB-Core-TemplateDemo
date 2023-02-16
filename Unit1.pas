97178e788ac05e8e137f0cdf0909b9b8823e7814
500Foods <andrew@500foods.com>
2023-02-16 14:48:38 -0800

    Updated Bootstrap
    - Removed Bootstrap version included in TMS WEB Bootstrap application template
    - Replaced with Bootstrap CDN version currently listed on the Bootstrap website.

--- Unit1.pas
+++ Unit1.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   System.SysUtils, System.Classes, JS, Web, WEBLib.Graphics, WEBLib.Controls,
-  WEBLib.Forms, WEBLib.Dialogs;
+  WEBLib.Forms, WEBLib.Dialogs, Vcl.Controls, Vcl.StdCtrls, WEBLib.StdCtrls;
 
 type
   TForm1 = class(TWebForm)
